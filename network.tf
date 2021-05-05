d94f84a2a4b12c86739b4cfd91e8173c3540e5dd
Your Name <you@example.com>
2021-05-05 16:43:40 +0700

    Update

--- network.tf
+++ network.tf
@@ -65,7 +65,7 @@ resource "aws_route_table" "private-rt" {
 resource "aws_subnet" "public-subnet" {
   count             = length(var.public_subnet_cidr)
   vpc_id            = aws_vpc.vpc.id
-  cidr_block        = element(var.public_subnet_cidr,count.index)
+  cidr_block        = element(var.public_subnet_cidr, count.index)
   # availability_zone = element(var.azs,count.index)
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
 
@@ -81,7 +81,7 @@ resource "aws_subnet" "public-subnet" {
 resource "aws_subnet" "private-subnet" {
   count             = length(var.private_subnet_cidr)
   vpc_id            = aws_vpc.vpc.id
-  cidr_block        = element(var.private_subnet_cidr,count.index)
+  cidr_block        = element(var.private_subnet_cidr, count.index)
   # availability_zone = element(var.azs,count.index)
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
 
@@ -96,13 +96,13 @@ resource "aws_subnet" "private-subnet" {
 # Associate route table to subnet
 resource "aws_route_table_association" "public" {
   count          = length(var.public_subnet_cidr)
-  subnet_id      = element(aws_subnet.public-subnet.*.id,count.index)
+  subnet_id      = element(aws_subnet.public-subnet.*.id, count.index)
   route_table_id = aws_route_table.public-rt.id
 }
 
 resource "aws_route_table_association" "private" {
   count          = length(var.private_subnet_cidr)
-  subnet_id      = element(aws_subnet.private-subnet.*.id,count.index)
+  subnet_id      = element(aws_subnet.private-subnet.*.id, count.index)
   route_table_id = aws_route_table.private-rt.id
 }
 
